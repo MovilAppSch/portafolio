469a6440b561139ed61eaaae5c38b5564779b114
Damian Schellemberg <sch.damian@hotmail.com>
2018-06-05 20:46:30 -0300

    servicio firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -9,14 +9,29 @@ export class InformacionService {
   info: any = {};
   cargada: boolean = false;
 
-  constructor(public http: Http) {
+  equipo: any = {};
+  cargada_sobreNosotros: boolean = false;
 
+  constructor(public http: Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     this.http.get("assets/data/info.pagina.json").
       subscribe(data => {
         console.log(data.json());
         this.cargada = true;
         this.info = data.json();
       });
+  }
 
+  public carga_sobre_nosotros() {
+    this.http.get("https://angularfirebase-82686.firebaseio.com/equipo.json").
+      subscribe(data => {
+        console.log(data.json());
+        this.cargada_sobreNosotros = true;
+        this.equipo = data.json();
+      });
   }
 }
